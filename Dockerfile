4298c2d7e84acd153a2972939c98228e1cddaa32
daniel-he-dev <danielhe.mlp@gmail.com>
2021-02-25 18:23:43 +0000

    Deploying to gh-pages from  @ 614805f50e9939b05b747f61a85c5e142797b1e2 🚀

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:lts
- WORKDIR /FEC
- COPY . .
- RUN npm install
- RUN npm run build
- CMD ["node", "server.js"]
--- testing/QuestionsAndAnswers.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import QuestionsAndAnswers from '../client/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx';
-
-test('tests tests', () => {
-    
-});
